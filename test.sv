7bfc604e5fbef70793a3019a4a1c20293ae835d9
NguyenHaiMinh-UIT <74281044+NguyenHaiMinh-UIT@users.noreply.github.com>
2023-10-22 16:13:21 +0700

    first push

---
+++ test.sv
@@ -0,0 +1,19 @@
+class Data;
+    rand bit [31:0] A;
+    rand bit [31:0] B;
+    bit [1:0] opcode;
+    constraint limit {
+        A inside {[0:8]};
+        B inside {[0:4]};
+        B < 2;
+    }
+    virtual function referamce_calc(A, B, opcode);
+        
+    endfunction //new()
+endclass //Data:
+
+
+
+program test;
+    
+endprogram : test
